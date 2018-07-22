32ff61f6b8c1fcb2791699ea6262faad495f2a25
Python Magus <github@redwaratah.com>
2018-07-22 10:23:57 +1000

    Added GPL headers prior to release to Freelancer to do CSS enhancements

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <? 
+/*
+ * 
+ *  This file is part of TWCOoR.
+ * 
+ *  TWCOoR is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+ *  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  TWCOoR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License along with TWCOoR. 
+ *  If not, see http://www.gnu.org/licenses/.
+ * 
+ */
 require "inc.php"; 
 $loggedIn = array_key_exists('userId', $_SESSION) ? $_SESSION['userId'] : '';
 
